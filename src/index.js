b510d4d1632ecb8bf95bd583359ea4c9e813ce94
Kitti Wateesatogkij <lgmcben@gmail.com>
2019-07-08 15:04:23 +0700

    refactor: Rename component

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 import styles from './styles.css'
 
-export default class ExampleComponent extends Component {
+export default class ScrollableTabs extends Component {
   static propTypes = {
     text: PropTypes.string,
     numTabs: PropTypes.number
