20715842fdd5b898a90829a198fdc09610dbaa91
Kitti Wateesatogkij <lgmcben@gmail.com>
2019-07-08 17:38:19 +0700

    feat: Update example to render tabs

--- example/src/App.js
+++ example/src/App.js
@@ -3,10 +3,19 @@ import React, { Component } from 'react'
 import ScrollableTabs from 'react-scrollable-tabs'
 
 export default class App extends Component {
+
+  
+  
   render () {
+    const tabs = [
+      { name: 'tab 1'},
+      { name: 'tab 2'},
+      { name: 'tab 3'}
+    ];
+    
     return (
       <div>
-        <ScrollableTabs numTabs={5} />
+        <ScrollableTabs tabs={tabs} />
       </div>
     )
   }
