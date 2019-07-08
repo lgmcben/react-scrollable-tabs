d6f08b97b34640a0f221a82332d6eb0a2a1cbf60
Kitti Wateesatogkij <lgmcben@gmail.com>
2019-07-08 17:37:20 +0700

    test: Fix shallow rendering bug by providing starting props
    Also alter props in unit test

--- src/test.js
+++ src/test.js
@@ -5,9 +5,13 @@ import ScrollableTabs from './'
 
 describe('ScrollableTabs', () => {
 
+  const props = {
+    tabs: []
+  }
+
   let wrapper;
 
-  beforeEach(() => wrapper = shallow(<ScrollableTabs />));
+  beforeEach(() => wrapper = shallow(<ScrollableTabs {...props} />));
 
   it('should render a <div />', () => {
     expect(wrapper.find('div').length).toEqual(1);
@@ -15,14 +19,20 @@ describe('ScrollableTabs', () => {
 
   it('should render x number of tabs according to tabs prop', () => {
     const tabs = [
+      { name: 'tab 0'},
       { name: 'tab 1'},
       { name: 'tab 2'},
-      { name: 'tab 3'}
+      { name: 'tab 3'},
+      { name: 'tab 4'},
+      { name: 'tab 5'}
     ];
 
     wrapper.setProps({
       tabs
     });
+
+    // console.log('wrapper.debug()', wrapper.debug());
+
     expect(wrapper.find('.tab').length).toEqual(tabs.length);
   });
 
