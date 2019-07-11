c56ad8dc4653fcc7cd73d9ce5c8f20f37173fe0e
Kitti Wateesatogkij <lgmcben@gmail.com>
2019-07-11 11:10:03 +0700

    fix: Reference error caused when converting to functional component

--- src/index.js
+++ src/index.js
@@ -2,29 +2,32 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import styles from './styles.css'
 
-const renderTabs = () => {
-  const tabs = this.props.tabs.map((item, index) => {
-    return (
-      <div className='tab' key={index}>
-        tab {item.name}
-      </div>
-    );
-  })
-  return tabs;
-}
-
-export default ScrollableTabs = () => {
+const ScrollableTabs = (props) => {
   // static propTypes = {
   //   tabs: PropTypes.array
   // }
 
     const {
       tabs
-    } = this.props
+    } = props
     
     return (
       <div className={styles.test}>
-        {this.renderTabs()}
+        {renderTabs(tabs)}
       </div>
     );
 }
+
+
+const renderTabs = (tabs) => {
+  const tabsJsx = tabs.map((item, index) => {
+    return (
+      <div className='tab' key={index}>
+        tab {item.name}
+      </div>
+    );
+  })
+  return tabsJsx;
+}
+
+export default ScrollableTabs;
