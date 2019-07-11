2960942174e0c2ef95155a5300fadd1575576d63
Kitti Wateesatogkij <lgmcben@gmail.com>
2019-07-11 14:36:58 +0700

    feat: Simulate situation when we need a lot of tabs

--- example/src/App.js
+++ example/src/App.js
@@ -10,7 +10,20 @@ export default class App extends Component {
     const tabs = [
       { name: 'tab 1'},
       { name: 'tab 2'},
-      { name: 'tab 3'}
+      { name: 'tab 3'},
+      { name: 'tab 4'},
+      { name: 'tab 5'},
+      { name: 'tab 6'},
+      { name: 'tab 7'},
+      { name: 'tab 8'},
+      { name: 'tab 9'},
+      { name: 'tab 10'},
+      { name: 'tab 11'},
+      { name: 'tab 12'},
+      { name: 'tab 13'},
+      { name: 'tab 14'},
+      { name: 'tab 15'},
+      { name: 'tab 16'}
     ];
     
     return (
