442f21e0a5dff40062757291d5e77c0100e7fc2c
Kitti Wateesatogkij <lgmcben@gmail.com>
2019-07-15 17:18:14 +0700

    test: Add snapshot testing

--- src/test.js
+++ src/test.js
@@ -13,27 +13,8 @@ describe('ScrollableTabs', () => {
 
   beforeEach(() => wrapper = shallow(<ScrollableTabs {...props} />));
 
-  it('should render a <div />', () => {
-    expect(wrapper.find('div').length).toEqual(1);
-  });
-
-  it('should render x number of tabs according to tabs prop', () => {
-    const tabs = [
-      { name: 'tab 0'},
-      { name: 'tab 1'},
-      { name: 'tab 2'},
-      { name: 'tab 3'},
-      { name: 'tab 4'},
-      { name: 'tab 5'}
-    ];
-
-    wrapper.setProps({
-      tabs
-    });
-
-    // console.log('wrapper.debug()', wrapper.debug());
-
-    expect(wrapper.find(Tab).length).toEqual(tabs.length);
+  it('renders correctly', () => {
+    expect(wrapper).toMatchSnapshot();
   });
 
 })
